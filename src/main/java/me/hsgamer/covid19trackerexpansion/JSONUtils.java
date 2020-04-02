a5ed421568472a8dc0cf1f23e8762cab93e9360a
HSGamer <huynhqtienvtag@gmail.com>
2020-04-02 09:13:58 +0700

    user agent

--- src/main/java/me/hsgamer/covid19trackerexpansion/JSONUtils.java
+++ src/main/java/me/hsgamer/covid19trackerexpansion/JSONUtils.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
+import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
 import org.json.JSONObject;
 
@@ -24,10 +25,11 @@ public class JSONUtils {
   }
 
   public static JSONObject readJsonFromUrl(String url) throws IOException {
-    try (InputStream is = new URL(url).openStream()) {
-      BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
-      String jsonText = readAll(rd);
-      return new JSONObject(jsonText);
-    }
+    URLConnection openConnection = new URL(url).openConnection();
+    openConnection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/74.0");
+    InputStream is = openConnection.getInputStream();
+    BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+    String jsonText = readAll(rd);
+    return new JSONObject(jsonText);
   }
 }
