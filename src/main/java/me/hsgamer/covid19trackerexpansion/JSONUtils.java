38b912c8a96a51b6fd80d142820a7afd485f4c9f
HSGamer <huynhqtienvtag@gmail.com>
2020-04-02 21:15:04 +0700

    small change

--- src/main/java/me/hsgamer/covid19trackerexpansion/JSONUtils.java
+++ src/main/java/me/hsgamer/covid19trackerexpansion/JSONUtils.java
@@ -2,7 +2,6 @@ package me.hsgamer.covid19trackerexpansion;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
@@ -25,9 +24,10 @@ public class JSONUtils {
   public static JSONObject readJsonFromUrl(String address) throws IOException {
     URL url = new URL(address);
     URLConnection openConnection = url.openConnection();
-    openConnection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
-    InputStream is = openConnection.getInputStream();
-    BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+    openConnection.addRequestProperty("User-Agent",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
+    openConnection.connect();
+    BufferedReader rd = new BufferedReader(new InputStreamReader(openConnection.getInputStream()));
     String jsonText = readAll(rd);
     return new JSONObject(jsonText);
   }
