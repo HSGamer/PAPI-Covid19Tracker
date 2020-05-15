6778f662847b26fb58c2d26280a3c82c8c4f3f1b
HSGamer <huynhqtienvtag@gmail.com>
2020-05-15 14:07:47 +0700

    no longer need this

--- src/main/java/me/hsgamer/covid19trackerexpansion/task/impl/CountryStats.java
+++ src/main/java/me/hsgamer/covid19trackerexpansion/task/impl/CountryStats.java
@@ -1,9 +1,6 @@
 package me.hsgamer.covid19trackerexpansion.task.impl;
 
-import java.io.IOException;
-import java.util.logging.Level;
 import me.hsgamer.covid19trackerexpansion.JSONUtils;
-import me.hsgamer.covid19trackerexpansion.Main;
 import me.hsgamer.covid19trackerexpansion.task.Stats;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.json.JSONObject;
@@ -19,15 +16,9 @@ public class CountryStats extends Stats {
 
   @Override
   public void run() {
-    try {
-      JSONObject jsonObject = JSONUtils.readJsonFromUrl(url);
-      if (jsonObject != null) {
-        setValue(jsonObject.getJSONArray("countrydata").getJSONObject(0));
-      }
-    } catch (IOException e) {
-      if (Main.isDebug()) {
-        plugin.getLogger().log(Level.WARNING, "Error when fetching data", e);
-      }
+    JSONObject jsonObject = JSONUtils.readJsonFromUrl(url);
+    if (jsonObject != null) {
+      setValue(jsonObject.getJSONArray("countrydata").getJSONObject(0));
     }
   }
 }
--- src/main/java/me/hsgamer/covid19trackerexpansion/task/impl/GlobalStats.java
+++ src/main/java/me/hsgamer/covid19trackerexpansion/task/impl/GlobalStats.java
@@ -1,9 +1,6 @@
 package me.hsgamer.covid19trackerexpansion.task.impl;
 
-import java.io.IOException;
-import java.util.logging.Level;
 import me.hsgamer.covid19trackerexpansion.JSONUtils;
-import me.hsgamer.covid19trackerexpansion.Main;
 import me.hsgamer.covid19trackerexpansion.task.Stats;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.json.JSONObject;
@@ -18,15 +15,9 @@ public class GlobalStats extends Stats {
 
   @Override
   public void run() {
-    try {
-      JSONObject jsonObject = JSONUtils.readJsonFromUrl(URL);
-      if (jsonObject != null) {
-        setValue(jsonObject.getJSONArray("results").getJSONObject(0));
-      }
-    } catch (IOException e) {
-      if (Main.isDebug()) {
-        plugin.getLogger().log(Level.WARNING, "Error when fetching data", e);
-      }
+    JSONObject jsonObject = JSONUtils.readJsonFromUrl(URL);
+    if (jsonObject != null) {
+      setValue(jsonObject.getJSONArray("results").getJSONObject(0));
     }
   }
 }
